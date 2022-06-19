54f849b9a49bd033404ff296a8509ec541a9bf74
Prateek Pai <paiprateek123@gmail.com>
2022-06-19 19:07:42 +0530

    Ability to Add New Contact to Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,9 +6,23 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook(2);
+            Console.WriteLine("Enter the Size of the Address Book:");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n                                        \n");
+
+            Console.WriteLine("Size of the Address Book is :"+size);
+            Console.WriteLine("...................................");
+            Console.WriteLine("\n                                        \n");
+
+            Console.WriteLine("Press Enter to Add the COntact in Address Book ");
+            string Press = Console.ReadLine();
+
+            AddressBook addressBook = new AddressBook(size);
             addressBook.AddNewContact();
+
+            Console.WriteLine("The Details of the First Contact in Address Book is:");
             addressBook.PrintAllContact();
+            
         }
     }
 }
